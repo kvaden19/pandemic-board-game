58a54fe55b385f671a1586a52be5c4ca50b001fc
kvaden19 <klvaden@gmail.com>
2020-04-17 08:28:15 -0400

    refactor classes into separate files proof of concept on gameboard (map) using matplotlib

--- map.py
+++ map.py
@@ -1,27 +1,59 @@
+'''
+Author: kvaden
+15 April 2020
+v1.0
+'''
+
 import matplotlib.pyplot as plt
 
-fig, ax = plt.subplots()
-ax.get_xaxis().set_visible(False)
-ax.get_yaxis().set_visible(False)
-ax.set_xlim(0,20)
-ax.set_ylim(0,10)
+class Map:
+    def __init__(self, city_dict=None, routes=None, cities=None, players=None):
+        self.city_dict = city_dict
+        self.routes = routes
+        self.cities = cities
+        self.players = players
 
-#cities
-city_x = [2,5,6,7,8,9]
-city_y = [8,9,6,9,6,9]
-labels = ['SFN', 'CHI', 'ATL', 'MTL', 'WDC', 'NYC']
-cubes = [0,0,1,0,3,0]
-players = ['','K','','','','E']
+    def draw(self):
+        # Set up the Axes object
+        fig, ax = plt.subplots()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        ax.set_xlim(0,20)
+        ax.set_ylim(0,10)
 
-#routes
-route_x = [2,5,6,8,9,7,5,7,8]
-route_y = [8,9,6,6,9,9,9,9,6]
+        # Unpack the city locations and their labels
+        city_x = []
+        city_y = []
+        labels = []
+        for label, coordinates in self.city_dict.items():
+            labels.append(label)
+            city_x.append(coordinates[0])
+            city_y.append(coordinates[1])
 
-plt.scatter(city_x,city_y,c='blue')
-plt.plot(route_x,route_y)
-for i, label in enumerate(labels):
-    ax.annotate(label, (city_x[i], city_y[i]))
-for i, value in enumerate(cubes):
-    ax.annotate(value, (city_x[i], city_y[i])) #need to use additional args to place the value elsewhere
-# annotate players on their locations
-plt.show()
+        # Set up the routes between cities
+        route_x = []
+        route_y = []
+        for label in self.routes:
+            route_x.append(self.city_dict[label][0])
+            route_y.append(self.city_dict[label][1])
+
+        # Create the plot
+        plt.scatter(city_x, city_y, c='blue')
+        plt.plot(route_x, route_y)
+        for i, label in enumerate(labels):
+            ax.annotate(label, (city_x[i], city_y[i]))
+
+        for player in self.players:
+            ax.annotate(player.name, (self.city_dict[player.location][0], self.city_dict[player.location][1]))
+
+        for city in self.cities:
+            if 'blue' in city.cubes:
+                ax.annotate(city.cubes['blue'], (self.city_dict[city.name][0], self.city_dict[city.name][1]))
+            if 'red' in city.cubes:
+                ax.annotate(city.cubes['red'], (self.city_dict[city.name][0], self.city_dict[city.name][1]))
+            if 'yellow' in city.cubes:
+                ax.annotate(city.cubes['yellow'], (self.city_dict[city.name][0], self.city_dict[city.name][1]))
+            if 'black' in city.cubes:
+                ax.annotate(city.cubes['black'], (self.city_dict[city.name][0], self.city_dict[city.name][1]))
+
+        plt.show()
